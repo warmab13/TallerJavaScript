2ab869928c18c39d8adccf68c7e09c9750cfab7a
Alonso S <alonsosalcido13@gmail.com>
2015-11-13 01:54:49 -0700

    Fourth commit, a dynamic template, and the binding with meteor and angular.

--- app.js
+++ app.js
@@ -1,20 +1,8 @@
+
 if(Meteor.isClient){
 	angular.module('socially', ['angular-meteor']);
 
-	angular.module('socially').controller('PartiesListCtrl', ['$scope', function($scope){
-		$scope.parties = [
-      	{
-        	'name': 'Dubstep-Free Zone',
-        	'description': 'Can we please just for an evening not listen to dubstep.'
-      	},
-      	{
-        	'name': 'All dubstep all the time',
-        	'description': 'Get it on!'
-      	},
-      	{
-        	'name': 'Savage lounging',
-        	'description': 'Leisure suit required. And only fiercest manners.'
-      	}
-    	];
+	angular.module('socially').controller('PartiesListCtrl', ['$scope', '$meteor', function($scope, $meteor){
+		$scope.parties = $meteor.collection(Parties);
 	}]);
 }
