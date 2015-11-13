546ba42f5628d3ba540ca106a10d5a6d4cb70f19
Alonso S <alonsosalcido13@gmail.com>
2015-11-13 03:10:33 -0700

    Seventh commit from the tutorial, in this part the meteor app can change the data from the partyDetails view using save, and reset buttons from the server.

--- app.js
+++ app.js
@@ -36,7 +36,20 @@ if(Meteor.isClient){
 	}]);
 
 	angular.module("socially").controller("PartyDetailsCtrl",  function($scope, $stateParams, $meteor) {
-		  	$scope.party = $meteor.object(Parties, $stateParams.partyId);
+		  	$scope.party = $meteor.object(Parties, $stateParams.partyId, false);
+
+		  	$scope.save = function(){
+		  		$scope.party.save().then(function(numberOfDocs){
+		  			console.log('save success doc affected ', numberOfDocs);
+		  		}, function(error){
+		  			console.log('save error ', error);
+		  		});
+		  	};
+
+		  	$scope.reset = function(){
+		  		$scope.party.reset();
+		  	};
+
 		  });
 
 	
